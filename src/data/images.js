e14abb201153ea108a0ae4fa8d2e33c4fc645ade
GabVoz13 <gpv5085@psu.edu>
2025-11-13 15:58:59 -0500

    added images into code

--- src/data/images.js
+++ src/data/images.js
@@ -1,5 +1,8 @@
 import bunny from "../images/bunny.jpeg";
 import fawn from "../images/piebald.jpeg";
+import Bee from "../images/bee.JPG";
+import mushroom from "../images/mushroom.JPG";
+import sky from "../images/sky.jpeg";
 
 export const images = [
     {
@@ -15,5 +18,26 @@ export const images = [
         title: "Piebald Fawn",
         caption: "Distinctive piebald markings in the meadow.",
         tags: ["deer", "piebald", "wildlife"],
+    },
+    {
+        id: 3,
+        src: Bee,
+        title: "Bee on yellow flower",
+        caption: "A bee sits on a yellow flower with its wings closed",
+        tags: ["bee", "yellow flower"],
+    },
+    {
+        id: 4,
+        src: mushroom,
+        title: "mushroom",
+        caption: "A mushroom surrounded by grass",
+        tags: ["mushroom", "grass", "fungi"],
+    },
+    {
+        id: 5,
+        src: sky,
+        title: "Sunset",
+        caption: "An orange sunset",
+        tags: ["sunset", "sky", "orange"],
     },
 ];
